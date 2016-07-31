0a7b755a39284274e5973c7b77bee03dfcbeb514
Ryan Hodin <ryan951357@gmail.com>
2016-07-30 20:29:34 -0700

    Add data part of main

--- Android random clicker/Android random clicker.cpp
+++ Android random clicker/Android random clicker.cpp
@@ -1,15 +1,26 @@
 // Android random clicker.cpp : Defines the entry point for the console application.
-//
+// This should not be trusted with high access: it accepts the path to adb and executes it without verification.
 
 #include "stdafx.h"
+#define chdir _chdir
 
 void usage(const char* prog);
+unsigned randint(unsigned max);
 
 int main(int argc, char* argv[])
 {
-	if (argc != 4)
+	if (argc != 5)
 		usage(argv[0]);
-	c
+	_chdir(argv[1]);
+	unsigned x_max(atoi(argv[2]));
+	unsigned y_max(atoi(argv[3]));
+	unsigned num(atoi(argv[4]));
+	for (unsigned i = 0; i < num; ++i)
+	{
+		unsigned x(randint(x_max));
+		unsigned y(randint(y_max));
+
+	}
     return 0;
 }
 
